67e6f9e5a7e057376eda577c1321a74d4fd027cb
Шанин Артём Юрьевич <joymefistoggc@gmail.com>
2019-11-26 00:56:30 +0300

    S-3 added some text

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="me">
-    <h1>Hello! It's ME!</h1>
+    <h1>Hello! It's ME! MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!</h1>
     <img src="../assets/me.png" alt="me">
   </div>
 </template>
