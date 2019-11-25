839b47cffef6898b2932f68f9f4ed9dc88621344
Шанин Артём Юрьевич <joymefistoggc@gmail.com>
2019-11-26 01:04:37 +0300

    S-4 added some images

--- src/views/Me.vue
+++ src/views/Me.vue
@@ -2,5 +2,7 @@
   <div class="me">
     <h1>Hello! It's ME! MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!</h1>
     <img src="../assets/me.png" alt="me">
+    <img src="../assets/me.png" alt="me">
+    <img src="../assets/me.png" alt="me">
   </div>
 </template>
